86dc5981dda2d66023cf09d64edb263c3df9a5a1
Erik Sanchez <erik.sape@gmail.com>
2020-09-13 16:23:00 -0500

    refactor(backend): support for optional arguments on graphqql

--- backend/lib/resolvers.js
+++ backend/lib/resolvers.js
@@ -37,11 +37,11 @@ module.exports = {
         }
     }),
     Query: {
-        createDocument: async(obj, {title, content}) => {
+        createDocument: async() => {
             const database = new PGConnection();
             const client = await database.client.connect();
             const query = 'INSERT INTO documents(title, content) VALUES ($1, $2) RETURNING id, title, content, updated_at';
-            const values = [title, content];
+            const values = ['new document', '# new document'];
             const document = await client.query(query, values);
             await client.end();
 
@@ -57,11 +57,20 @@ module.exports = {
             return documents.rows;
         },
 
+        readDocument: async (obj, {id}) => {
+            const database = new PGConnection();
+            const client = await database.client.connect();
+            const documents = await client.query('SELECT id, title, content, updated_at FROM documents where id=$1', [id]);
+            await client.end();
+            return documents.rows[0];
+        },
+
         updateDocument: async (obj, {id, title, content}) => {
             const database = new PGConnection();
             const client = await database.client.connect();
+            const row = (await client.query('SELECT id, title, content FROM documents where id=$1', [id])).rows[0]
             const query = 'UPDATE documents SET title=$1, content=$2, updated_at=now() where id=$3 RETURNING id, title, content, updated_at';
-            const values = [title, content, id];
+            const values = [title||row.title, (content ? decodeURI(content):row.content), id];
             const document = await client.query(query, values)
             await client.end();
 
